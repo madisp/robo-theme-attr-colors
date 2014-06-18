a7359ab3125c391d93a5ae0db14df208a3ab6b6f
Madis Pink <madis.pink@gmail.com>
2014-06-18 18:23:13 -0400

    Add actual testcode to both espresso and robo

--- src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
+++ src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
@@ -1,6 +1,9 @@
 package com.example.activity;
 
 import android.app.Activity;
+import android.content.res.TypedArray;
+import android.util.TypedValue;
+import com.example.R;
 import com.example.robolectric.DeckardActivity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,5 +20,19 @@ public class DeckardActivityRobolectricTest {
     public void testSomething() throws Exception {
         Activity activity = Robolectric.buildActivity(DeckardActivity.class).create().get();
         assertTrue(activity != null);
+    }
+
+    @Test
+    public void testColorBg() throws Exception {
+        Activity activity = Robolectric.buildActivity(DeckardActivity.class).create().get();
+        activity.setTheme(R.style.MyBlackTheme);
+        TypedArray arr = activity.obtainStyledAttributes(new int[] { android.R.attr.windowBackground });
+        TypedValue value = new TypedValue();
+        arr.getValue(0, value);
+        arr.recycle();
+
+        assertThat(value.type).isGreaterThanOrEqualTo(TypedValue.TYPE_FIRST_COLOR_INT)
+            .isLessThanOrEqualTo(TypedValue.TYPE_LAST_COLOR_INT);
+        assertThat(value.data).isEqualTo(activity.getResources().getColor(android.R.color.black));
     }
 }
--- src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
+++ src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
@@ -1,9 +1,13 @@
 package com.example.espresso.activity;
 
+import android.app.Activity;
+import android.content.res.TypedArray;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.TypedValue;
 import com.example.R;
 import com.example.robolectric.DeckardActivity;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
@@ -14,10 +18,10 @@ import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMat
 public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<DeckardActivity> {
 
     @SuppressWarnings("deprecation")
-     public DeckardEspressoTest() {
-       // This constructor was deprecated - but we want to support lower API levels.
-       super("com.example.activity", DeckardActivity.class);
-     }
+    public DeckardEspressoTest() {
+        // This constructor was deprecated - but we want to support lower API levels.
+        super("com.example.activity", DeckardActivity.class);
+    }
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -28,5 +32,18 @@ public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<Deckar
     public void testCheckText() {
         onView(withId(R.id.text))
             .check(matches(withText("Hello Espresso!")));
-      }
+    }
+
+    public void testColorBg() throws Exception {
+        Activity activity = getActivity();
+        activity.setTheme(R.style.MyBlackTheme);
+        TypedArray arr = activity.obtainStyledAttributes(new int[] { android.R.attr.windowBackground });
+        TypedValue value = new TypedValue();
+        arr.getValue(0, value);
+        arr.recycle();
+
+        assertThat(value.type).isGreaterThanOrEqualTo(TypedValue.TYPE_FIRST_COLOR_INT)
+            .isLessThanOrEqualTo(TypedValue.TYPE_LAST_COLOR_INT);
+        assertThat(value.data).isEqualTo(activity.getResources().getColor(android.R.color.black));
+    }
 }
